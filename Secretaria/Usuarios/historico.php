5735968ba2a8f1232a080fcdc4326c471a59cd1f
JColbertm <j.g.colbertm@gmail.com>
2016-05-16 01:46:30 -0400

    actualizacion

--- Secretaria/Usuarios/historico.php
+++ Secretaria/Usuarios/historico.php
@@ -4,28 +4,78 @@
     <div class="card">
       <div class="card-content">
         <div class="row">
+
           <form class="col s12">
             <div class="row">
-              <div class="input-field col s6">
-                <input disabled value="Oscar Paz Gonzales" id="last_name" type="text" class="validate">
-                <label for="last_name">Nombre y Apellido</label>
-              </div>
-              <div class="input-field col s6">
-                <input disabled value="5633233" id="last_name" type="text" class="validate">
-                <label for="last_name">CI</label>
-              </div>
+              <center><h4>Afiliacion de Socio Historico</h4></center>
+            </div>
+            <div class="row">
+              <div class="col s6">
+                <label><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de Afiliacion:</label> 
+                <input type="date" class="datepicker" required>
+              </div>              
             </div>
             <div class="row">
               <div class="input-field col s6">
-                <input disabled placeholder="12/04/2016" id="disabled" type="text" class="validate">
-                <label for="disabled">Fecha Afiliacion</label>
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-user" aria-hidden="true"></i> Carnet de Identidad:</label>
+              </div>              
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-user" aria-hidden="true"></i> Nombres:</label>
+              </div>       
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label>Apellidos:</label>
+              </div>              
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-map-marker" aria-hidden="true"></i> Direccion:</label>
+              </div>              
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-phone" aria-hidden="true"></i> Nro. Telefono:</label>
               </div>
               <div class="input-field col s6">
-                <input disabled placeholder="ASDSA12412ASD" id="password" type="text" class="validate">
-                <label for="password">Nueva Contraseña</label>
-              </div>
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-mobile" aria-hidden="true"></i> Nro. Celular:</label>
+              </div>                
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="email" class="validate" required>
+                <label><i class="fa fa-envelope-o" aria-hidden="true"></i> Correo Electronico:</label>
+              </div>              
+            </div>
+            
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-money" aria-hidden="true"></i> Total Ganado:</label>
+              </div>    
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-money" aria-hidden="true"></i> Liquido Pagable:</label>
+              </div>              
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                <input type="text" class="validate" required>
+                <label><i class="fa fa-money" aria-hidden="true"></i> Aporte Mensual:</label>
+              </div>              
+              <div class="col s6">
+                <label><i class="fa fa-calendar" aria-hidden="true"></i> Fecha de Afiliacion:</label> 
+                <input type="date" class="datepicker" required>
+              </div>              
             </div>
           </form>
+
         </div>
         <div class="row">
           <div class="col offset-m9">
